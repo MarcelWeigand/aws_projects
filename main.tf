786e51fb08990fb514f2bde1ed433fdc36009464
Weigand <Marcel.Weigand@t-systems.com>
2025-02-10 18:24:57 +0100

    new terraform

--- main.tf
+++ main.tf
@@ -3,13 +3,29 @@ provider "aws" {
   region = "us-east-1" # Replace with your preferred AWS region
 }
 
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-marcel-1994"
+    key            = "state/terraform.tfstate"  # Path inside the bucket
+    region         = "us-east-1"
+    encrypt        = true                        # Encrypt state at rest
+  }
+}
+
+
 # Create an S3 bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-marcel-aws-tf-240794" # Replace with a unique bucket name
-  acl    = "private"
+  bucket = "example-marcel-aws-tf-240794-testbucket" # Replace with a unique bucket name
+  
 
   tags = {
     Name        = "ExampleBucket"
     Environment = "New"
   }
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-demo-bucket-marcel-240794"
 }
